741ec663139e4a27950cd8fd2de4e2250afba274
grace <123graciev@graces-MBP.fios-router.home>
2020-02-09 18:18:02 -0500

    groovy

--- js/app.js
+++ js/app.js
@@ -74,16 +74,14 @@ function getWinner() {
   let winner = null;
 
   winningConditions.forEach(function(condition, index) {
-    if (
-      board[condition[0]] &&
-      board[condition[0]] === board[condition[1]] &&
-      board[condition[1]] === board[condition[2]]
-    ) {
+    if (board[condition[0]] &&
+        board[condition[0]] === board[condition[1]] &&
+        board[condition[1]] === board[condition[2]]) {
       winner = board[condition[0]];
       if(winner === "X"){
         pointsX++;
         document.getElementById("scoreboard1").innerHTML = pointsX;
-      }
+    }
       if(winner === "O"){
         pointsY++;
         document.getElementById("scoreboard2").innerHTML = pointsY;

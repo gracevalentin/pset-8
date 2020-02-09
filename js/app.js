1653db2dd0e270eb77821158efaacc90046b7855
grace <123graciev@graces-MBP.fios-router.home>
2020-02-09 11:13:52 -0500

    very cool

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
 
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 
-let board
+let board;
 let turn;
+let win;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
@@ -17,13 +29,18 @@ window.onload = init;
 
 document.getElementById("board").onclick = takeTurn;
 
+document.getElementById("reset-button").onclick = init;
+
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
 function render() {
   board.forEach(function(mark, index) {
-    squares[index].textContent = mark;    // writes an X or an O on board
-   });
- }
+    squares[index].textContent = mark;
+  });
+
+  message.textContent =
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+}
 
 function init() {
   board = [
@@ -33,16 +50,39 @@ function init() {
   ];
 
   turn = "X";
+  win = null;
 
-  render();   // we'll write this later
+  render();
 }
 
 function takeTurn(e) {
-  let index = squares.findIndex(function(square) {
-    return square === e.target;
-  });
-  board[index] = turn;
-  turn = turn === "X" ? "O" : "X";  // alternate turns
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
 
-  render();
+    if (board[index] === "") {
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
+    }
+  }
+}
+
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+  return winner ? winner : board.includes("") ? null : "T";
 }
